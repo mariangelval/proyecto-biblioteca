3f2eb3fdbdeec0aeea44566321b2a5b088b1fcb3
VaninaBlas <vanyabrilconblas@gmail.com>
2023-10-19 12:01:35 -0300

    Creando proyecto C#

---
+++ src/Biblio.Core/Autor.cs
@@ -0,0 +1,14 @@
+﻿namespace Biblio.Core;
+
+public class Autor
+{
+    public ushort idAutor {get; set;}
+    public required string nombre {get;set;}
+    public required string apellido {get;set;}
+}
+public class Titulos
+{
+    public ushort idAutor {get; set;}
+    public required string nombre {get;set;}
+    public ushort anio_primero {get;set;}
+}
---
+++ src/Biblio.Core/Titulo.cs
@@ -0,0 +1,7 @@
+namespace Biblio.Core;
+public class Titulo
+{
+    public ushort idAutor { get; set; }
+    public required string nombre { get; set; }
+    public ushort anio_primero { get; set; }
+}
