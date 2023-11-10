5f5f01a612942c56993e84e80100e215d1273c7e
VaninaBlas <vanyabrilconblas@gmail.com>
2023-11-10 09:16:09 -0300

    programando test

---
+++ src/Biblio.AdoDapp/Repos/RepoTitulo.cs
@@ -0,0 +1,21 @@
+using System.Data;
+using Biblio.Core;
+using Biblio.Core.Persistencia.Repositorios;
+using Dapper;
+
+namespace Biblio.AdoDapp.Repos
+{
+    public class RepoTitulo: Repo
+    {
+        private const string _queryTitulos =
+        @"SELECT * FROM Titulos";
+        private const string _altaTitulo =
+            @"";
+        public RepoTitulo(IDbConnection conexion)
+            : base(conexion) {}
+        public void Alta(Titulo elemento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
